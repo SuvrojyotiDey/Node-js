86c981575eda44a332f5ce6c99ee14868a105f78
SuvrojyotiDey <samrat.dey.india@gmail.com>
2024-09-30 06:33:24 +0530

    Updated missing files

--- Node_Js_Mongo_Production_Setup/README.md
+++
@@ -1,4 +0,0 @@
-Docker Commands :-
-
-docker build -t backend-app:dev -f docker/development/Dockerfile .
-docker run --rm -it -v ${pwd}:/usr/src/backend-app -v /usr/src/backend-app/node_modules -p 3000:3000 backend-app:dev
--- Node_Js_Mongo_Production_Setup/dist/constant/responseMessage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SUCCESS: `The operation ha been successful`,
-    SOMETHING_WENT_WRONG: `Something went wrong`,
-    NOT_FOUND: (entity) => `${entity} not found`,
-    TO_MANY_MESSAGES: `To many messages`
-};
-//# sourceMappingURL=responseMessage.js.map
